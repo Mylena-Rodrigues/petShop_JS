16e31bb8100e97365af4d7af7ff3152c470ef977
Mylena Rodrigues <mylenawz@yahoo.com.br>
2021-03-25 10:53:16 -0300

    Função e objetos

---
+++ index.js
@@ -0,0 +1,50 @@
+const nomePetshop = "PETSHOP DECOLA";
+
+let pets = [{
+    nome: 'Mike',
+    tipo: 'Cachorro',
+    sexo: 'Masculina',
+    idade: 8,
+    raca: 'vira-lata',
+    peso: 10,
+    tutor: 'Mylena',
+    contato: '(81) 98682-6693',
+    vacinado: true,
+    servicos: ['banho', 'patacure']
+},
+{
+    nome: 'Theodoro',
+    tipo: 'Gato',
+    idade: 3,
+    raca: 'vira-lata',
+    peso: 4.5,
+    sexo: 'Masculina',
+    tutor: 'Gilvaneide',
+    contato: '(81) 98675-6789',
+    vacinado: true,
+    servicos: ['tosa', 'exercicio']
+},
+{
+    nome: 'Frida',
+    tipo: 'Gato',
+    idade: 8,
+    raca: 'vira-lata',
+    peso: 3,
+    sexo: 'Feminino',
+    tutor: 'Mylena',
+    contato: '(81) 98682-6693',
+    vacinado: true,
+    servicos: ['banho', 'tosa']
+}
+];
+
+//function () {} vira () => {}
+
+const listarPets = () => {
+    for(let i = 0; i<pets.length; i++){
+        console.log(pets[i].nome);
+    }
+}
+
+listarPets();
+//console.log(pet);
